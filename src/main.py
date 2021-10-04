20ed2027f871a37d8bd05b2ff311cd1ffe9fd4cd
0Nera <slizen.can@gmail.com>
2021-10-04 22:57:39 +0300

    Create footer and favicon

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, request, make_response, redirect
+from flask import Flask, url_for, render_template, request, make_response, redirect, session
 
 app = Flask(
 	__name__, 
@@ -6,35 +6,71 @@ app = Flask(
 	static_folder = "static"
 )
 
+app.secret_key = "super secret key"
+app.debug = True
+
+
 def has_no_empty_params(rule):
-    defaults = rule.defaults if rule.defaults is not None else ()
-    arguments = rule.arguments if rule.arguments is not None else ()
-    return len(defaults) >= len(arguments)
+	defaults = rule.defaults if rule.defaults is not None else ()
+	arguments = rule.arguments if rule.arguments is not None else ()
+	return len(defaults) >= len(arguments)
+
 
 @app.route('/')
 def index():
-    return make_response("Hello World! Your IP is {} and you are using {}<br><a href='./site-map'>Sitemap</a>".format(request.remote_addr, request.user_agent), 200)
+	links = []
+	for rule in app.url_map.iter_rules():
+		if "GET" in rule.methods and has_no_empty_params(rule):
+			url = url_for(rule.endpoint, **(rule.defaults or {}))
+			links.append((url, rule.endpoint))
+	return make_response(
+		render_template("index.html", 
+			links=reversed(links),
+			ip=request.remote_addr,
+			browser=request.user_agent
+			), 
+		200
+		)
+
+
+@app.route('/session-test')
+def session_test():
+	if 'visits' in session:
+    		session['visits'] = session.get('visits') + 1  # чтение и обновление данных сессии
+	else:
+		session['visits'] = 1  # настройка данных сессии
+	return make_response(
+		render_template("session-test.html", 
+			visits=session.get('visits')
+			), 
+		200
+		)
+
+@app.route('/session-pop')
+def session_pop():
+	session.pop('visits', None)
+	return redirect(
+		url_for("session_test"), 
+		code=200
+		)
+
+@app.route('/about')
+def about():
+	return make_response(
+		render_template("about.html"
+			),
+		200
+		)
 
 
 @app.route("/redirect/<url>")
 def redirect_to(url):
-    return make_response(redirect(url), 200)
+	return make_response(
+		redirect(url), 
+		200
+		)
 
-
-
-
-
-@app.route("/site-map")
-def site_map():
-    links = []
-    for rule in app.url_map.iter_rules():
-        if "GET" in rule.methods and has_no_empty_params(rule):
-            url = url_for(rule.endpoint, **(rule.defaults or {}))
-            links.append((url, rule.endpoint))
-    return make_response(render_template("all_links.html", links=links), 200)
 
 
 if __name__ == "__main__":
-    app.run(
-        debug=True
-        )
+	app.run()
