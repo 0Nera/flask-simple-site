9b5f58f9a80e20dee82a05075f7aa8b973ec7087
0Nera <slizen.can@gmail.com>
2021-10-05 13:36:26 +0300

    Remove SQLAlchemy

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from flask import Flask, url_for, render_template, request, make_response, redir
 from flask_login import LoginManager, UserMixin, login_required, login_user, current_user
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask_sqlalchemy import SQLAlchemy
 from PIL import Image, ImageDraw
 from datetime import datetime
 
@@ -24,7 +23,6 @@ limiter = Limiter(
 
 
 app.config['SECRET_KEY'] = 'a really really really really long secret key'
-db = SQLAlchemy(app)
 img_counter = 0
 login_manager = LoginManager(app)
 login_manager.init_app(app)
@@ -44,26 +42,25 @@ def login():
     return render_template('login.html')
 
 
-@limiter.limit("12/minute")
+@limiter.limit("10/minute")
 @login_manager.user_loader
 @app.route('/gen-img')
 def gen_img():
 	now = datetime.now()
 	global img_counter
 	img_counter += 1
-	text = "Hello, World!\nGenerated: " + str(img_counter) + "\nServer time: " + str(now) + "\nThis site created on Flask framework\n\n©2021 Aren Elchinyan\nSource code: https://github.com/0Nera"
+	text = "Hello, World!\nGenerated: " + str(img_counter) + "\nServer time: " + str(now) + "\nThis site created on Flask framework\n\nSource code: https://github.com/0Nera\n©2021 Aren Elchinyan"
 	color = (0, 0, 120)
 	img = Image.new('RGB', (256, 128), color)
 	imgDrawer = ImageDraw.Draw(img)
 	imgDrawer.text((10, 10), text)
-	img.save("static/img/temp.png")
+	img.save("static/temp/temp.png")
 	return make_response(
 		render_template("gen-img.html"), 
 		200
 		) 
 
 
-@limiter.limit("1/second")
 @login_manager.user_loader
 @app.route('/')
 def index():
